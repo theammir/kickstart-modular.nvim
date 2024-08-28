b7af65fb1239626b67f9687fe83f3de480bf1021
theammir <the@ammir.dev>
2024-08-28 20:39:42 +0300

    fix: proper vim-startify header

--- lua/custom/plugins/dashboard.lua
+++ lua/custom/plugins/dashboard.lua
@@ -11,12 +11,14 @@ return {
     end
 
     -- Set up Startify with a custom header
-    vim.g.startify_custom_header = vim.fn['startify#center'](read_header_file 'lua/header')
+    vim.g.startify_custom_header = vim.fn['startify#center'](read_header_file(vim.fn.stdpath 'config' .. '/lua/header'))
 
     -- Disable default lists (bookmarks, sessions, etc.)
-    vim.g.startify_lists = {}
+    vim.g.startify_lists = { { type = 'bookmarks', header = { '' } } }
+    vim.g.startify_enable_special = 0
 
     -- Prevent session autoloading
     vim.g.startify_session_autoload = 0
+    vim.g.startify_change_to_dir = 0
   end,
 }
